591aec573cb39d57b5f2f02a5b143d04b5e13ef8
testgitIos12 <maksutovsamir635@gmail.com>
2022-01-06 22:23:38 +0600

    Initial Commit

---
+++ Homework1.3/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Homework1.3
+//
+//  Created by Samir Maksutov on 6/1/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
